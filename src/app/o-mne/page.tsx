1a3228e9b2c1d74224dac2494eb49c21d7a9a84f
Erik690 <ErikKern@s.zochova.sk>
2024-09-24 11:19:03 +0200

    basic routing added

---
+++ src/app/o-mne/page.tsx
@@ -0,0 +1,15 @@
+// src/app/o-mne/page.tsx
+
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+
+export const metadata = { title: "O mne  | ZochovaTube" }
+
+export default function Home() {
+ 
+  return (
+        <Container>
+            <Typography> Stránka o mne </Typography>
+        </Container>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+// src/app/page.tsx
+
+
+import Typography from "@mui/material/Typography";
+
+export const metadata = { title: "Domov | ZochovaTube" }
+
+export default function Home() {
+ 
+  return (
+
+      <Typography> Domovská stránka </Typography>
+      
+  );
+}
